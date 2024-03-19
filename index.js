78611a2278a556653aba68dfd7f8519f943b1e03
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-03-19 11:38:47 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(){
+    if ('distances are below 42nd street:')
+    return 1;
+}
+
+function distanceFromHqInFeet(){
+    if ('if distance is below 42nd street:')
+    return 264;
+}
+
+function distanceTravelledInFeet(){
+    if ('destination is below distance:')
+    return 1320;
+}
+
+function calculatesFarePrice(start, destination){
+    if ('between 0 and 400 feet:')
+    return 'gives customer a free sample';
+}
+
+function calculatesFarePrice(start, destination){
+    if ('feet travelled is between 400 and 2000:')
+    return 'charges 2 cents per foot';
+}
+
+function calculatesFarePrice(start, destination){
+    if ('over 2000 feet:')
+    return 'charges 25 dollars';
+}
+
+function calculatesFarePrice(start, destination){
+    if ('over 2500 feet:')
+    return 0;
+}
+
+
